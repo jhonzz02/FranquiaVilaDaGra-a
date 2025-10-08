d88e72e54f9f47fb701a6d5ec6baa984c785eaf0
João Victor Calasans <142369193+jhonzz02@users.noreply.github.com>
2025-10-08 19:29:47 -0300

    initial commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="flex justify-between items-center bg-red-900">
+            <div>LOGO</div>
+            <div>CONTATO</div>
+        </header>
+    )
+}
---
+++ src/app/components/hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <div className="min-h-[420px] flex justify-center items-center">
+      <div className="flex flex-col">
+        <h1>Vila Da Graça</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quisquam
+          porro labore ipsum vel, qui reprehenderit, nobis nam at enim vitae
+          numquam fugiat sit dolorem! Incidunt quod perferendis repellat quidem!
+        </p>
+        <div className="flex gap-8">
+          <button>Torne-se um franqueado</button>
+          <button>Conheça o modelo da franquia</button>
+        </div>
+      </div>
+    </div>
+  );
+}
