1c5e41f802aa23592675f3880b23b8fc2b6def74
João Victor Calasans <142369193+jhonzz02@users.noreply.github.com>
2025-10-15 19:40:01 -0300

    feat header and hero style

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
 export default function Header() {
-    return (
-        <header className="flex justify-between items-center bg-red-900">
-            <div>LOGO</div>
-            <div>CONTATO</div>
-        </header>
-    )
+  return (
+    <header className="flex justify-between items-center bg-[#453109] backdrop-blur-sm px-8 py-4">
+      <div>
+        <Image
+          src="/logo.jpg"
+          alt="Logo Vila da Graça"
+          width={80}
+          height={80}
+          priority
+          className="rounded-full"
+        />
+      </div>
+      <div style={{ fontFamily: '"Times New Roman", serif' }}  className="font-bold text-xl">CONTATO</div>
+    </header>
+  );
 }
--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,7 +1,8 @@
+
 export default function Hero() {
   return (
-    <div className="min-h-[420px] flex justify-center items-center">
-      <div className="flex flex-col">
+    <div className="min-h-[420px] flex gap-8 justify-center items-center bg-[#E6DAA8]">
+      <div className="flex flex-col text-black">
         <h1>Vila Da Graça</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quisquam
@@ -12,6 +13,10 @@ export default function Hero() {
           <button>Torne-se um franqueado</button>
           <button>Conheça o modelo da franquia</button>
         </div>
+      </div>
+      <div style={{ fontFamily: '"Times New Roman", serif' }} >
+        <h1 className="text-[#453109] text-[36px] text-center">VILA DA <br/> GRAÇA</h1>
+        <h2 className="text-[#7f580a] text-[23px] text-center">A SUA VILA</h2>
       </div>
     </div>
   );
